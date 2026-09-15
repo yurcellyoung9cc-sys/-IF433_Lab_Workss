7717c8ec423dbdce8b5c4d4782d20470285186c8
yurcellyoung9cc-sys <yurcellyoung9cc@gmail.com>
2026-09-15 16:26:50 +0700

    week01: add calculateStatus function

--- src/oop_00000054092_YurcellY/week01/GradingSystem.kt
+++ src/oop_00000054092_YurcellY/week01/GradingSystem.kt
@@ -1,5 +1,7 @@
 package oop_00000054092_YurcellY.week01
 
+fun calculateStatus(score: Int) = if (score > 75) "Lulus" else "Tidak Lulus"
+
 fun main () {
     val name: String = "John Thor"
     val score: Int = 80
@@ -12,5 +14,7 @@ fun main () {
         in 70..79 -> "C"
         else -> "D"
     }
-    println("grade kamu \$grade")
+    println("grade kamu $grade")
+
+    println("Status: ${calculateStatus(score)}")
 }
