5db8fcd8f7a721394586b6f2dbd4ff9c44222bd1
yurcellyoung9cc-sys <yurcellyoung9cc@gmail.com>
2026-09-16 17:34:08 +0700

    week02: implement user input and pre-validation

---
+++ src/week02/Main.kt
@@ -0,0 +1,30 @@
+package week02
+
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+
+    println("--- APLIKASI PMB UMN ---")
+
+    print("Masukkan Nama: ")
+    val name = scanner.nextLine()
+
+    print("Masukkan NIM (Wajib 5 Karakter): ")
+    val nim = scanner.next()
+
+    scanner.nextLine() // Bersihkan buffer newline (Penyakit klasik Scanner!)
+
+    // Validasi di sisi pemanggil (Main)
+    if (nim.length != 5) {
+        println("ERROR: Pendaftaran dibatalkan. NIM harus 5 karakter!")
+        // Program berhenti di sini untuk mahasiswa ini, tidak membuat objek
+    } else {
+        print("Masukkan Jurusan: ")
+        val major = scanner.nextLine()
+
+        // Instansiasi Objek karena data sudah aman
+        val s1 = Student(name, nim, major)
+        println("Status: Pendaftaran Selesai.")
+    }
+}
