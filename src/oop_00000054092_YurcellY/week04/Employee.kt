c7d24840d1cf9cb2db5e43f6e17dd834e97c0466
yurcellyoung9cc-sys <yurcellyoung9cc@gmail.com>
2026-09-23 17:22:59 +0700

    week04: (task 2) create parent class Employee

--- src/oop_00000054092_YurcellY/week04/Employee.kt
+++ src/oop_00000054092_YurcellY/week04/Employee.kt
@@ -5,7 +5,6 @@ open class Employee(val name: String, val baseSalary: Int) {
     open fun work() {
         println("$name sedang bekerja.")
     }
-
     open fun calculateBonus(): Int {
         return (baseSalary * 0.10).toInt()
     }
