9b0603f6653e51086c8641cfe0dafe19865bb55a
yurcellyoung9cc-sys <yurcellyoung9cc@gmail.com>
2026-09-23 17:16:13 +0700

    week04: create Car class extending Vehicle

--- src/oop_00000054092_YurcellY/week04/Car.kt
+++ src/oop_00000054092_YurcellY/week04/Car.kt
@@ -2,7 +2,6 @@ package week04
 
 // Car "Is-A" Vehicle. Parameter brand dilempar ke atas melalui Vehicle(brand)
 open class Car(brand: String, val numberOfDoors: Int) : Vehicle(brand) {
-
     fun openTrunk() {
         println("Bagasi mobil $brand dengan $numberOfDoors pintu dibuka.")
     }
